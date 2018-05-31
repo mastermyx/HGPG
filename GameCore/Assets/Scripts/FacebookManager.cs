bed524d12248a74a177c2d6782d18347e6ba60b0
Arthur Hatchiguian <arthur.h@greenpandagames.com>
2018-05-31 09:55:33 +0200

    Game Core

---
+++ GameCore/Assets/Scripts/FacebookManager.cs
@@ -0,0 +1,44 @@
+﻿using Facebook.Unity;
+using UnityEngine;
+
+public class FacebookManager : MonoBehaviour {
+
+	void Awake()
+    {
+		// The object (and script) will stay on scene change
+		DontDestroyOnLoad(this.gameObject);
+
+        CheckFacebookActivation();
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+		// "status false" means "OnApplicationResume"
+        if (false == pauseStatus)
+            CheckFacebookActivation();
+    }
+
+    // Using the SDKs initialization methods
+	private void CheckFacebookActivation()
+    {
+        if (FB.IsInitialized)
+            FB.ActivateApp();
+        else
+        {
+            // Handle FB.Init
+            FB.Init(() => {
+                FB.ActivateApp();
+            });
+        }
+    }
+
+    public static void TrackEvent(string eventName)
+    {
+		FB.LogAppEvent(eventName);
+	}
+
+    public static void TrackEvent(string eventName, int paramValue)
+    {
+		FB.LogAppEvent(eventName, paramValue);
+    }
+}
